cd9f0a342738be11a1e9dc12b0edb09cc9c5d2ee
ammarkamran031106 <ammarkamran2006@gmail.com>
2024-09-27 00:14:08 +0500

    question7.c

--- Lab task 5/question7.c
+++ Lab task 5/question7.c
@@ -1,1 +1,10 @@
 // question 7
+#include <stdio.h>
+int main()
+{
+int x;
+printf("enter any positive number:");
+scanf("%d",&x);
+(x%10==0)?printf("last digit is zero"):printf("last digit is non-zero");
+return 0;
+}

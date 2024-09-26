d7d06093dfb8909116d8bdb24143bdaf26725b18
ammarkamran031106 <ammarkamran2006@gmail.com>
2024-09-27 00:13:25 +0500

    question6.c

--- Lab task 5/question6.c
+++ Lab task 5/question6.c
@@ -1,10 +1,3 @@
 // question 6
-#include <stdio.h>
-int main()
-{
-int x;
-printf("enter any positive number:");
-scanf("%d",&x);
-(x%10==0)?printf("last digit is zero"):printf("last digit is non-zero");
-return 0;
-}
+The x=5 > 8 ? So it’s false so it will go for else which is1=!2<5? So here first, comparison have a getter precedence that why 2<5 yes it’s true so output 1 and then check 1=!1 so it’s false so again else statement will be printed which is 30.Output is 30
+
